1253b35da2b59affcefc15badd9d4cdff8af217f
231998-Laiba <231998@students.au.edu.pk>
2026-03-10 15:02:18 +0500

    LAB TASK 1 Student Information Card App Completed

---
+++ student-card-app/src/components/StudentCard.js
@@ -0,0 +1,17 @@
+import './StudentCard.css';
+
+function StudentCard(props) {
+  return (
+    <div
+      className="student-card"
+      style={{ backgroundColor: props.color }}
+    >
+      <h2>{props.name}</h2>
+      <p><strong>Roll No:</strong> {props.rollNo}</p>
+      <p><strong>Department:</strong> {props.department}</p>
+      <p><strong>University:</strong> {props.university}</p>
+    </div>
+  );
+}
+
+export default StudentCard;
